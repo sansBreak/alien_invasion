ceea7fb2c48bdca88482cda0bbe8db1d3dc639bd
sansBreak <1273836074@qq.com>
2021-09-04 22:02:14 +0800

    1.新增子弹与射击功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -19,7 +20,6 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
 
-
         # 通过设置类中的配置，来设置游戏的分辨率
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
@@ -28,6 +28,7 @@ class AlienInvasion:
 
         # 引入实例ship
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         # 设置背景色
         self.bg_color = (230, 230, 230)
@@ -39,6 +40,8 @@ class AlienInvasion:
             self._check_events()
             # 时刻更新飞船位置
             self.ship.update()
+            # 更新子弹位置
+            self._update_bullets()
             # 通过构筑辅助方法_update_screen，来更新屏幕
             self._update_screen()
 
@@ -66,6 +69,8 @@ class AlienInvasion:
         # 按下“q"键时退出游戏
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """响应松开"""
@@ -74,12 +79,31 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入编组bullets中"""
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除消失的子弹"""
+        # 更新子弹的位置
+        self.bullets.update()
+
+        # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
         # 更新屏幕上的图像，并切换到新屏幕
         # 每次循环时都重绘屏幕
         self.screen.fill(self.settings.bg_color)
         # 将飞船绘制到屏幕上
         self.ship.blitme()
+        # 将子弹绘制到屏幕上
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
